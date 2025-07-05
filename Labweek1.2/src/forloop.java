342c90af989396666a0392cd36ec201a5c5a56b9
Natthakritta <nattakritta_ao@cmu.ac.th>
2025-07-05 20:37:49 +0700

    Turtle commit

---
+++ Labweek1.2/src/forloop.java
@@ -0,0 +1,93 @@
+import java.awt.*;
+
+public class forloop {
+    public static void main(String[] args) {
+        Turtle Danny = new Turtle();
+        Danny.speed(0);
+        Danny.setPosition(0,0);
+
+        for (int j = 0; j < 36; j++) {
+            double length = 5;
+            for (int i = 0; i < 60; i++) {
+                int colorIndex = (i / 15) % 4;
+
+                if (colorIndex == 0) {
+                    Danny.penColor(Color.red);
+                } else if (colorIndex == 1) {
+                    Danny.penColor(Color.magenta);
+                } else if (colorIndex == 2) {
+                    Danny.penColor(Color.pink);
+                } else {
+                    Danny.penColor(Color.yellow);
+                }
+
+                Danny.forward(length);
+                Danny.left(5);
+                length += 0.5;
+            }
+
+            Danny.forward(10);
+            Danny.backward(10);
+
+            Danny.right(10);
+        }
+
+        for (int i = 0; i < 72; i++) {
+            Turtle Mickey = new Turtle();
+            Mickey.speed(0);
+            Mickey.up();
+            Mickey.setPosition(-322, 185);
+            Mickey.down();
+
+            for (int j = 0; j < i * 5; j++) {
+                Mickey.left(1); // จำลองการหมุน angle
+            }
+
+            Mickey.forward(425);
+        }
+
+        Danny.speed(0); // เร็วสุด
+        Danny.width(5);
+        double xCenter = -322;
+        double yCenter = 185;
+
+        double a = 1305;
+        double b = 470;
+
+        int steps = 200;
+
+        // upper
+        Danny.up();
+        for (int i = 0; i <= steps; i++) {
+            double t = (double) i / steps;
+            double x = xCenter + a * (2 * t - 1); // จาก -a ถึง +a
+            double relativeX = x - xCenter;
+            double y = yCenter + b * Math.sqrt(1 - (relativeX * relativeX) / (a * a));
+
+            if (i == 0) {
+                Danny.setPosition(x, y);
+                Danny.down();
+            } else {
+                Danny.setPosition(x, y);
+            }
+        }
+
+        //lower eyelid
+        Danny.up();
+        for (int i = steps; i >= 0; i--) {
+            double t = (double) i / steps;
+            double x = xCenter + a * (2 * t - 1); // จาก +a กลับ -a
+            double relativeX = x - xCenter;
+            double y = yCenter - b * Math.sqrt(1 - (relativeX * relativeX) / (a * a));
+
+            if (i == steps) {
+                Danny.setPosition(x, y);
+                Danny.down();
+            } else {
+                Danny.setPosition(x, y);
+            }
+        }
+
+        Danny.up();
+    }
+}
